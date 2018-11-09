3f4a02de5b3f5e41da2ede2f3d2ceff441509e5e
qiaoqiao10001 <qiaoqiao10001@gmail.com>
2018-11-09 10:06:36 +0800

    拆分组件

--- src/item.jsx
+++ src/item.jsx
@@ -3,8 +3,6 @@ import React,{Component} from 'react'
 export default class Item extends Component{
 
     handleClick = () => {
-        // alert(this.props.idx)
-        // const idx =
         this.props.handleDelete(this.props.idx)
     }
 
