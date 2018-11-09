40c520375588990fc3c1b6099982a1bfaa80d9bf
qiaoqiao10001 <qiaoqiao10001@gmail.com>
2018-11-09 09:59:31 +0800

    拆分组件

--- src/todolist.jsx
+++ src/todolist.jsx
@@ -1,4 +1,5 @@
 import React,{Fragment} from 'react'
+import Item from './item'
 
 export default class TodoList extends React.Component{
     state = {
@@ -25,10 +26,9 @@ export default class TodoList extends React.Component{
         
     }
 
-    handleDelete(idx) {
+    handleDelete = (idx) => {
         console.log(this)
         console.log(idx)
-        debugger
         const list = [...this.state.list]
         list.splice(idx,1)
         this.setState({list})
@@ -40,10 +40,18 @@ export default class TodoList extends React.Component{
             <Fragment>
                 <input type="text" value={inputValue} onChange={this.handleInputChange}/>
                 <button onClick={this.handleClick}>添加</button>
-
                 <ul>
                    {
-                       list.map((item,idx) => <li key={idx} onClick={this.handleDelete.bind(this,idx)}>{item}</li>)
+                       list.map((item,idx) =>
+                           <div key={idx}>
+                               <Item
+                                   content={item}
+                                    idx={idx}
+                                   handleDelete={this.handleDelete}
+                               />
+                           </div>
+                           /*<li key={idx} onClick={this.handleDelete.bind(this,idx)}>{item}</li>*/
+                       )
                    }
                 </ul>
             </Fragment>
